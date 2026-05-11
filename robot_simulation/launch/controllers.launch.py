49e15862d87175239227285bf19a3f69245b1b1f
ErsaGunTosun <ersagunfatihdev@gmail.com>
2026-05-11 22:54:53 +0300

    REMOVE all relay tf, cmd_vel and odom relay removed

--- robot_simulation/launch/controllers.launch.py
+++ robot_simulation/launch/controllers.launch.py
@@ -17,34 +17,7 @@ def generate_launch_description():
         output='screen'
     )
     
-    tf_relay = Node(
-        package='topic_tools',
-        executable='relay',
-        name='tf_odometry_relay',
-        arguments=['/mecanum_drive_controller/tf_odometry', '/tf'],
-        output='screen'
-    )
-
-    cmd_vel_relay = Node(
-        package='topic_tools',
-        executable='relay',
-        name='cmd_vel_relay',
-        arguments=['/cmd_vel', '/mecanum_drive_controller/reference_unstamped'],
-        output='screen'
-    )
-
-    odom_relay = Node(
-        package='topic_tools',
-        executable='relay',
-        name='odom_relay',
-        arguments=['/mecanum_drive_controller/odometry', '/odom'],
-        output='screen'
-    )
-    
     return LaunchDescription([
         joint_state_broadcaster_spawner,
         mecanum_controller_spawner,
-        tf_relay,
-        cmd_vel_relay,
-        odom_relay,
     ])
